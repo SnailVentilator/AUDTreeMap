00c1ba544778b7317f64c890c01d82f340277c6e
Fabian <fabian.paul.gurtner@gmail.com>
2020-03-30 23:48:08 +0200

    Implement basic put test
    [skip ci]

--- src/test/java/net/htlgrieskirchen/aud2/map/MyMapTest.java
+++ src/test/java/net/htlgrieskirchen/aud2/map/MyMapTest.java
@@ -1,8 +1,36 @@
 package net.htlgrieskirchen.aud2.map;
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.assertEquals;
 
 public class MyMapTest {
+	private Map<String, String> myMap;
+	private Map<String, String> treeMap;
 
+	@Before
+	public void prepareMaps() {
+		myMap = new MyMap<>();
+		treeMap = new TreeMap<>();
+	}
+
+	@Test
+	public void basicPutTest() {
+		executeAndCompare(map -> {
+			map.put(someString[0], someString[1]);
+		});
+	}
+
+	public static String[] someString = {"404", "Internet", "Not", "Found"};
+
+	public void executeAndCompare(Consumer<Map<String, String>> runnable) {
+		runnable.accept(myMap);
+		runnable.accept(treeMap);
+		assertEquals(myMap, treeMap);
+	}
 }
