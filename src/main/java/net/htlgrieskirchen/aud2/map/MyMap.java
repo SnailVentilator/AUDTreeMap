e4a18afaa8c0bacabbbcc17583d5d42b9f81e4a7
Fabian <fabian.paul.gurtner@gmail.com>
2020-03-30 20:07:45 +0200

    Add method stubs

--- src/main/java/net/htlgrieskirchen/aud2/map/MyMap.java
+++ src/main/java/net/htlgrieskirchen/aud2/map/MyMap.java
@@ -1,5 +1,67 @@
 package net.htlgrieskirchen.aud2.map;
 
-public class MyMap {
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
+public class MyMap<K,V> implements Map<K,V> {
+	@Override
+	public int size() {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public boolean isEmpty() {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public V get(Object key) {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public V put(K key, V value) {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public V remove(Object key) {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> m) {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public void clear() {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public Set<K> keySet() {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public Collection<V> values() {
+		throw new IllegalStateException("Not yet implemented!");
+	}
+
+	@Override
+	public Set<Entry<K, V>> entrySet() {
+		throw new IllegalStateException("Not yet implemented!");
+	}
 }
