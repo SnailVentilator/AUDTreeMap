b3889876f9dd36f4b40052bae910b298e0f6cc12
Andreas <aigner.andi05@gmail.com>
2020-03-31 19:57:43 +0200

    Implement put and size Implements #19 Implements #25

--- src/main/java/net/htlgrieskirchen/aud2/map/MyMap.java
+++ src/main/java/net/htlgrieskirchen/aud2/map/MyMap.java
@@ -2,12 +2,15 @@ package net.htlgrieskirchen.aud2.map;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class MyMap<K extends Comparable<K>,V> implements Map<K,V> {
+	private MyEntry<K, V> root = null;
+
 	@Override
 	public int size() {
-		throw new IllegalStateException("Not yet implemented!");
+		return root.size();
 	}
 
 	@Override
@@ -33,7 +36,13 @@ public class MyMap<K extends Comparable<K>,V> implements Map<K,V> {
 
 	@Override
 	public V put(K key, V value) {
-		throw new IllegalStateException("Not yet implemented!");
+		MyEntry<K, V> entry = new MyEntry<>(key, value);
+		if(root == null) {
+			root = entry;
+			return null;
+		}else {
+			return root.put(key,value);
+		}
 	}
 
 	@Override
@@ -83,5 +92,46 @@ public class MyMap<K extends Comparable<K>,V> implements Map<K,V> {
 			this.key = key;
 			this.value = value;
 		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			MyEntry<?, ?> myEntry = (MyEntry<?, ?>) o;
+			return Objects.equals(key, myEntry.key);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(key);
+		}
+
+		public V put(K key, V value) {
+			MyEntry<K, V> entry = new MyEntry<>(key, value);
+			if(this.equals(entry)) {
+				V oldValue = this.value;
+				this.value = value;
+				return oldValue;
+			}
+			if(this.compareTo(entry) <= 0) {
+				if(this.left == null) {
+					this.left = entry;
+					return null;
+				}
+				return this.left.put(key,value);
+			}else {
+				if(this.right == null) {
+					this.right = entry;
+					return null;
+				}
+				return this.right.put(key,value);
+			}
+
+		}
+
+		public int size() {
+			return 1+ (left==null?0:left.size()) + (right==null?0:right.size());
+		}
+
 	}
 }
